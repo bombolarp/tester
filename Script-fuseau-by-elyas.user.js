20c88a5be5c130661612cde00a707da471a57ee5
bombolarp <decryptmail94@gmail.com>
2026-05-13 17:57:37 +0200

    Update Script-fuseau-by-elyas.user.js

--- Script-fuseau-by-elyas.user.js
+++ Script-fuseau-by-elyas.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tech by @elyas.tech Updated recently
 // @namespace    _
-// @version      9.2
+// @version      9.3
 // @description  Timeline modifier Script
 // @author       @elyas.tech
 // @match        https://app.triple-a.io/*
